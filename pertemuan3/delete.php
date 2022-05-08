f4b7cee4c36770842015d82eb4b8c17521def30c
naufalmuafa <42337245+naufalmuafa@users.noreply.github.com>
2022-05-08 14:33:46 +0700

    Add files via upload

---
+++ pertemuan3/delete.php
@@ -0,0 +1,26 @@
+<?php
+
+    // File json yang akan dibaca
+    $file = "anggota.json";
+
+    // Mendapatkan file json
+    $anggota = file_get_contents($file);
+
+    // Mendecode anggota.json
+    $data = json_decode($anggota, true);
+
+    // Membaca data array menggunakan foreach
+    foreach ($data as $key => $d){
+        // Hapus data kedua
+        if($d['no'] === 3){
+            // Menghapus data array sesuai dengan index
+            // Menggantinya dengan alamat baru
+            array_splice($data, $key, 3);
+        }
+    }
+
+    // Mengencode data menjadi json
+    $jsonfile = json_encode($data, JSON_PRETTY_PRINT);
+
+    // Menyimpan data ke dalam anggota.json
+    $anggota = file_put_contents($file, $jsonfile);
